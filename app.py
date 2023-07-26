ee157913490a1fe5ba364e4e710276211162625d
michael frederickson <mpfplus@gmail.com>
2023-07-26 13:21:20 -0600

    WIP: Done with skeleton for app.py and User class for models.py

--- app.py
+++ app.py
@@ -12,3 +12,34 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
+
+
+@app.get('/')
+def list_users():
+    """List users and show form to add user"""
+
+    users = Users.query.all()
+    return render_template("list.html", users=users)
+
+
+@app.post('/')
+def add_user():
+    """Add user and redirect to user list"""
+
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    image_url = request.form.get("image_url")
+
+    user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect(f'/{user.id}')
+
+
+@app.get('/<int:user_id>')
+def show_user(user_id):
+    """Display info on a single user"""
+
+    user = User.query.get_or_404(user_id)
+    return render_template("detail.html", user=user)
--- models.py
+++ models.py
@@ -1,1 +1,40 @@
 """Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+
+class User(db.Model):
+    """Contains descriptions and functionality for a User"""
+
+    __tablename__ = "users"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    first_name = db.Column(
+        db.String(25),
+        nullable=False
+    )
+
+    last_name = db.Column(
+        db.String(25),
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.String(),
+        nullable=True
+    )
